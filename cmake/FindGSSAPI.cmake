2ebde0e9f1b9a47ed83ebc0487dda5ec0701697a
dodo040 <dorian.ducournau@gmail.com>
2017-08-18 16:01:11 +0200

    little fix FindGSSAPI.cmake

--- cmake/FindGSSAPI.cmake
+++ cmake/FindGSSAPI.cmake
@@ -85,7 +85,6 @@ elseif(WIN32)
 endif()
 
 if(NOT GSS_FOUND) # not found by pkg-config. Let's take more traditional approach.
-  message(STATUS "not found by pkg-config. Take more traditional approach")
   find_file(_GSS_CONFIGURE_SCRIPT
       NAMES
           "krb5-config"
@@ -122,7 +121,7 @@ if(NOT GSS_FOUND) # not found by pkg-config. Let's take more traditional approac
   endif()
 
   # FIXME : fail to link Heimdal libraries using configure script, we do it "manually"
-  if(NOT "${_GSS_CONFIGURE_SCRIPT} " STREQUAL " " AND NOT ${_GSS_VENDOR} STREQUAL "Heimdal" AND GSS_FLAVOUR)
+  if(NOT "${_GSS_CONFIGURE_SCRIPT} " STREQUAL " " AND GSS_FLAVOUR AND NOT ${_GSS_VENDOR} STREQUAL "Heimdal")
     execute_process(
           COMMAND ${_GSS_CONFIGURE_SCRIPT} "--cflags" "gssapi"
           OUTPUT_VARIABLE _GSS_CFLAGS
